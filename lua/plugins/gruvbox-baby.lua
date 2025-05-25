52ab118238a007c89632b41b8e4295b152c05775
Rolf K <abstractstar@gmail.com>
2025-05-25 11:09:02 +0200

    Add gruvbox-baby colorscheme

---
+++ lua/plugins/gruvbox-baby.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox-baby
+  { "abstrakct/gruvbox-baby" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-baby",
+    },
+  },
+}
