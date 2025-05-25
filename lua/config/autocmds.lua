5108d3db21446a027bba3fa5046f2815b341c8cf
Rolf K <abstractstar@gmail.com>
2025-05-25 16:16:06 +0200

    Disable spell checker
    fuck that shit, i don't need it

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.diagnostic.disable(0)
   end,
 })
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- vim.api.nvim_del_augroup_by_name("wrap_spell")
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false -- fuck you spellchecker
+  end,
+})
